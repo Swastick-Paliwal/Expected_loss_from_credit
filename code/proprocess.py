261dd63ff5ef2a58a029a591ed128b7f86eeeb9a
Swastick-Paliwal <paliwal.swastick@gmail.com>
2025-05-07 23:53:07 +0530

    preprocessing done, working on feature_importance

--- code/proprocess.py
+++ code/proprocess.py
@@ -1,12 +1,7 @@
-#import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+from config import features_to_keep
 
-accepted=pd.read_csv('accepted_2007_to_2018Q4.csv')
-
-#looking at missing entries
-miss=accepted.isna().sum()
-missing_entries_sorted = miss.sort_values(ascending=False)
-print(missing_entries_sorted)
+#keeping only important entries as per mentor and chatgpt, this list is made from values with less than 20% of missing values 
+dataset = pd.read_csv('accepted_2007_to_2018Q4.csv', usecols=features_to_keep)
+dataset.to_csv('dataset.csv', index=False)
 
